c0640be738619921c7db3691d465041d2ed3d935
Amar <jeetamar496@gmail.com>
2024-03-01 00:10:39 +0530

    eyes position corrected

--- react-framer-motion/src/components/Eyes.jsx
+++ react-framer-motion/src/components/Eyes.jsx
@@ -7,16 +7,20 @@ const Eyes = () => {
          bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")]'> 
          <div className='flex absolute top-1/2 left-1/2 w-1/3 gap-10 -translate-x-[50%] -translate-y-[50%]'>
             <div className='flex items-center justify-center w-[15vw] h-[15vw] bg-white rounded-full'>
-                <div className='relative w-2/3 h-2/3 flex items-center justify-center rounded-full bg-zinc-950'>
-                <div className='line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]'> 
+                <div className='relative w-2/3 h-2/3 rounded-full bg-zinc-950'>
+                    {/* doubt in the below class property how to do this ?? */}
+                <div className='line absolute top-1/2 left-1/2 h-10  w-full -translate-x-[50%] -translate-y-[50%]'> 
                     <div className=' w-10 h-10 rounded-full bg-zinc-100'>
                     </div>  
                     </div>
                 </div>
             </div>
             <div className='flex items-center justify-center w-[15vw] h-[15vw] bg-white rounded-full'>
-                <div className=' w-2/3 h-2/3 flex items-center justify-center rounded-full bg-zinc-950'>
-                    <div className=' w-1/4 h-1/4 rounded-full bg-zinc-100'></div>
+            <div className='relative w-2/3 h-2/3 rounded-full bg-zinc-950'>
+                <div className='line absolute top-1/2 left-1/2 h-10  w-full -translate-x-[50%] -translate-y-[50%]'> 
+                    <div className=' w-10 h-10 rounded-full bg-zinc-100'>
+                    </div>  
+                    </div>
                 </div>
             </div>
          </div>
