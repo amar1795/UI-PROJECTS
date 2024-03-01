6c17b92320d60c88a080af2eb42324d683628b91
Amar <jeetamar496@gmail.com>
2024-03-01 21:09:20 +0530

    section 2 animation added

--- react-framer-motion/src/components/Marquee.jsx
+++ react-framer-motion/src/components/Marquee.jsx
@@ -1,11 +1,22 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 
 const Marquee = () => {
+  
   return (
     <div className=' w-full h-screen py-10 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
-      <div className='text border-t-2 border-b-2 border-zinc-300 flex justify-center pb-4 whitespace-nowrap overflow-hidden'>
-        <h1 className='text-[17vw] leading-none font-["Test Founders Grotesk X-cond Sm Semi Bold"] mr-4'>We are Ochi</h1>
-        <h1 className='text-[17vw] leading-none font-["Test Founders Grotesk X-cond Sm Semi Bold"]'>We are Ochi</h1>
+      <div className='text border-t-2 border-b-2 border-zinc-300 flex justify-center pb-4 whitespace-nowrap overflow-hidden '>
+        <motion.h1 initial={{x:"0"}} animate={{x:"-100%"}} transition={{repeat:Infinity,
+        ease:"linear",duration:8
+        }} className='text-[17vw] font-bold uppercase leading-none font-["Test Founders Grotesk X-cond Sm Semi Bold"] pr-20'>We are Ochi</motion.h1>
+
+        <motion.h1 initial={{x:"0"}} animate={{x:"-100%"}} transition={{repeat:Infinity,
+        ease:"linear",duration:8
+        }} className='text-[17vw] leading-none font-bold uppercase font-["Test Founders Grotesk X-cond Sm Semi Bold"] pr-20'>We are Ochi</motion.h1>
+
+      <motion.h1 initial={{x:"0"}} animate={{x:"-100%"}} transition={{repeat:Infinity,
+        ease:"linear",duration:8
+        }} className='text-[17vw] leading-none font-bold uppercase font-["Test Founders Grotesk X-cond Sm Semi Bold"] pr-20'>We are Ochi</motion.h1>
 
       </div>
     </div>
