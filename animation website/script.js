9afacc4564bd0cd792b5f9483c6dd6bccbae1693
Amar <jeetamar496@gmail.com>
2024-09-11 19:13:49 +0530

    page 2 animation added

--- animation website/script.js
+++ animation website/script.js
@@ -1,15 +1,60 @@
-let tl = gsap.timeline();
+function loading() {
+  let tl = gsap.timeline();
 
-tl.to("#yellow ",{
+  tl.to("#yellow1 ", {
     top: "-100%",
     duration: 0.5,
     delay: 0.5,
-    ease:"expo.out" 
-})
+    ease: "expo.out",
+  });
 
-tl.to("#loader ",{
-    top: "-100%",
+  // can also do like this
+  // tl.to("#loader h1 ",{
+  //     color:"white",
+
+  // },"anim")
+
+  tl.to(
+    "#loader video ",
+    {
+      top: "-100%",
+      duration: 0.5,
+      delay: 0.5,
+      ease: "expo.out",
+    },
+    "anim"
+  );
+
+  tl.to(
+    "#loader h1",
+    {
+      color: "black",
+      ease: "expo.out",
+      delay: 0.5,
+    },
+    "anim"
+  );
+  tl.to(
+    "#loader ",
+    {
+     display:"none",
+      ease: "expo.out",
+     
+    }
+  );
+
+
+ tl.to("#nav",{
+    display:"block",
+    ease: "expo.out",
+    // delay: 0.5,
     duration: 0.5,
-    delay: 0.5,
-    ease:"expo.out" 
-})
+ })
+
+}
+loading();
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("main"),
+  smooth: true,
+});
