f6f342775a4b1cb822c0f107d4937ae5a52e3ac7
Amar <jeetamar496@gmail.com>
2024-09-12 18:10:52 +0530

    page 2 animation completed

--- animation website/script.js
+++ animation website/script.js
@@ -70,3 +70,76 @@ elems.forEach((elem)=>{
     page2.style.backgroundImage=`url(${image})`;
   })
 })
+
+// let elems = document.querySelectorAll(".elem");
+// let page2 = document.querySelector("#page2");
+//  let moving= document.querySelectorAll(".moving");
+//  let h2 = document.querySelectorAll(".elem h2");
+//  let movingInH5 = document.querySelectorAll(".moving .moving-in h5");
+
+// let currentIndex = 0;
+// let interval;
+
+// function simulateMouseEnter(elem) {
+//   const image = elem.getAttribute("data-image");
+//   page2.style.backgroundImage = `url(${image})`;
+//   moving[currentIndex].style.transform = "translate(0, -40%) scaleY(1)";
+//   h2[currentIndex].style.fontStyle = "italic";
+//   movingInH5.forEach(h5 => {
+//     h5.style.opacity = "1";
+// });
+
+// }
+
+// function startAutoHover() {
+//   interval = setInterval(() => {
+//     if (currentIndex >= elems.length) {
+//       currentIndex = 0; 
+//     }
+    
+//     elems.forEach((elem, idx) => {
+//       if (idx !== currentIndex) {
+//         moving[idx].style.transform = "translate(0, -40%) scaleY(0)";
+//         h2[idx].style.fontStyle = "normal";
+//         movingInH5.forEach(h5 => {
+//           h5.style.opacity = "0";
+//       });
+//       }
+//     });
+    
+//     simulateMouseEnter(elems[currentIndex]);
+    
+//     currentIndex++;
+//   }, 1500); 
+// }
+
+// function removeHoverClass(elem) {
+  
+//   page2.style.backgroundImage = `url(${image})`;
+//   moving[currentIndex].style.transform = "translate(0, -40%) scaleY(0)";
+//   h2[currentIndex].style.fontStyle = "normal";
+//   movingInH5.forEach(h5 => {
+//     h5.style.opacity = "0";
+// });
+  
+// }
+
+// startAutoHover();
+
+// elems.forEach((elem, index) => {
+//   elem.addEventListener("mouseenter", () => {
+//     const image=elem.getAttribute("data-image");
+//     page2.style.backgroundImage=`url(${image})`;
+//     elems.forEach(e => removeHoverClass(e));
+    
+//     simulateMouseEnter(elem);
+//   });
+//   clearInterval(interval);
+
+//   elem.addEventListener("mouseleave", () => {
+//     removeHoverClass(elem);
+    
+//     startAutoHover();
+//   });
+// });
+
