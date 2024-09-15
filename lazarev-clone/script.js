01afa097633b7e8a13cb71c30c4c97a1d7a66358
Amar <jeetamar496@gmail.com>
2024-09-15 17:04:51 +0530

    navbar hovering animation issue corrected

--- lazarev-clone/script.js
+++ lazarev-clone/script.js
@@ -1,58 +1,71 @@
-let nav=document.querySelector('#main #center');
+let nav = document.querySelector('#main #center');
+let menuoptions = document.querySelectorAll('.option h4');
+let border_bottom = document.querySelector('#main #nav-bottom');
+let isAnimating = false;
 
-let menuoptions=document.querySelectorAll('.option h4');
-let border_bottom=document.querySelector('#main #nav-bottom');
+nav.addEventListener('mouseenter', function() {
+    // Kill any ongoing animation to prevent overlap
+    if (!isAnimating) {
+        isAnimating = true;
+        gsap.killTweensOf([border_bottom, menuoptions, "#center .option h4 span"]);
 
-nav.addEventListener('mouseenter',function(){
-
-    let tl=gsap.timeline();
-
-    tl.to(border_bottom,{
-        height:"20vh",
-         duration:0.3,
-        //  delay:0.5,
-     });
-    tl.to(menuoptions,{
-        display:'block',
-        duration:0.1,
-        // delay:0.5,    
-    });
-    tl.from("#center .option h4 span",{
-        // opacity:0,
-        y:40,      
-            stagger:{
-                amount:0.5,
+        let tl = gsap.timeline({
+            onComplete: () => {
+                isAnimating = false;  // Animation is done, reset the flag
             }
+        });
+
+        tl.to(border_bottom, {
+            height: "20vh",
+            duration: 0.3,
+        });
         
+        tl.to(menuoptions, {
+            display: 'block',
+            duration: 0.1,
+        });
         
-    })
-    
-   ;
+        tl.from("#center .option h4 span", {
+            y: 40,
+            stagger: {
+                amount: 0.5,
+            },
+            clearProps: "all"  // Clears properties after animation completes
+        });
+    }
 });
 
-
-nav.addEventListener('mouseleave',function(){
-
-    let tl=gsap.timeline();
-    tl.to("#center .option h4 span",{
-        
-        y:40,      
-            stagger:{
-                amount:0.5,
-            }
-        
-        
-    })
+nav.addEventListener('mouseleave', function() {
+   // Kill any ongoing animation to prevent overlap
   
-    tl.to(menuoptions,{
-        display:'none',
-        duration:0.1,
-    });
-    tl.to(border_bottom,{
-        height:"0vh",
-         duration:0.3,
-        //  delay:0.5,
-     });
-     
-   ;
+        isAnimating = true;
+        gsap.killTweensOf([border_bottom, menuoptions, "#center .option h4 span"]);
+
+        let gl = gsap.timeline({
+            onComplete: () => {
+                isAnimating = false;  // Animation is done, reset the flag
+            }
+        });
+
+        gl.to("#center .option h4 span", {
+            y: 40,
+            stagger: {
+                amount: 0.5,
+            },
+        });
+
+        gl.to(menuoptions, {
+            display: 'none',
+            duration: 0.1,
+        });
+
+        gl.to(border_bottom, {
+            height: "0vh",
+            duration: 0.1,
+            onComplete: function() {
+                // Reset text spans to initial position
+                gsap.set("#center .option h4 span", { y: 0 });
+            }
+        });
+    
 });
